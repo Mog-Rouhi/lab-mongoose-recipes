da38a70db5e4e2343f7120df1859efc1e0112a41
Mog Rouhi <moghadase.roohi@gmail.com>
2022-09-29 21:21:36 +0100

    complete all iterations

--- index.js
+++ index.js
@@ -17,6 +17,38 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const foodRecipe = {
+      title: "Vegetable Soup",
+      level: "Easy Peasy",
+      ingredients: ["carrots", "celery", "tomatoes", "potatoes", "green beans", "corn", "peas", "yellow onion", "garlic"],
+      cuisine: "Portuguese",
+      dishType: "soup",
+      duration: 40,
+      creator: "unknown",
+    }
+    return Recipe.create(foodRecipe);
+  })
+  .then(() => {
+    console.log("succeed")
+    return Recipe.insertMany(data);
+  })
+  .then((res) => {
+    res.forEach((recipe) => {
+      console.log(recipe.title);
+    })
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration: 100})
+  })
+  .then((response)=> {
+    console.log("successsfully updated duration for", response)
+  })
+  .then(() => {
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then(() => {
+    console.log("all steps done. time to close!")
+    mongoose.connection.close();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
